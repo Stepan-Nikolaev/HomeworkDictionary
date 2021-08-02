b4fac49eb7cb37336364913ffb73fc7c7f0eaf8c
Stepan-Nikolaev <me.ctenah@gmail.com>
2021-08-03 01:54:48 +0300

    Initial commit

---
+++ Figure.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkDictionary
+{
+    public class Figure
+    {
+        public int SideCount { get; set; }
+        public int SideLenght { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (!(obj is Figure))
+            {
+                return false;
+            }
+
+            Figure currentFigure = (Figure)obj;
+
+            return SideCount == currentFigure.SideCount && SideLenght == currentFigure.SideLenght;
+        }
+
+        public override int GetHashCode()
+        {
+            return SideCount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace HomeworkDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Figure> figuresList = new List<Figure>();
+            Dictionary<Figure, int> figuresDictionary = new Dictionary<Figure, int>();
+            Stopwatch timeList = new Stopwatch();
+            Stopwatch timeDictionary = new Stopwatch();
+            double timeOneOperationInList;
+            double timeOneOperationDectionary;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                figuresList.Add(new Figure() { SideCount = i, SideLenght = i });
+                figuresDictionary.Add(new Figure() { SideCount = i, SideLenght = i }, i);
+            }
+
+            Console.WriteLine("Введите любое число от 0 до 1000.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Figure searchingFigure = new Figure() { SideCount = userInput, SideLenght = userInput };
+
+            timeList.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                figuresList.Contains(searchingFigure);
+            }
+            timeList.Stop();
+            timeOneOperationInList = (double)timeList.ElapsedMilliseconds / 100000;
+            Console.WriteLine(timeOneOperationInList);
+
+            timeDictionary.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                figuresDictionary.ContainsKey(searchingFigure);
+            }
+            timeDictionary.Stop();
+            timeOneOperationDectionary = (double)timeDictionary.ElapsedMilliseconds / 100000;
+            Console.WriteLine(timeOneOperationDectionary);
+
+        }
+    }
+}
